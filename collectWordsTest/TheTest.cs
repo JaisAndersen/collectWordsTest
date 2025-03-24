cc5badde545df542506b1cba94eab67a04b671de
Jais Andersen <jais.h.sorensen@gmail.com>
2025-03-24 10:26:13 +0100

    Add project files.

---
+++ collectWordsTest/TheTest.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+
+namespace collectWordsTest
+{
+    [TestClass]
+    public sealed class TheTest
+    {
+        private static readonly string DriverDirectory = "c:\\WebDrivers";
+        private static IWebDriver driver;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
