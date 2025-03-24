9d07215eae91673a0769c52a75f74a95b57a8bf4
Jais Andersen <jais.h.sorensen@gmail.com>
2025-03-24 11:22:02 +0100

    test env. build

--- collectWordsTest/TheTest.cs
+++ collectWordsTest/TheTest.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 
 namespace collectWordsTest
 {
@@ -8,9 +10,28 @@ namespace collectWordsTest
         private static readonly string DriverDirectory = "c:\\WebDrivers";
         private static IWebDriver driver;
 
+        [ClassInitialize]
+        public static void setup(TestContext context)
+        {
+            driver = new EdgeDriver(DriverDirectory);
+        }
+
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            driver.Dispose();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+            string url = "http://127.0.0.1:5501/index.html";
+            driver.Navigate().GoToUrl(url);
+
+            Assert.AreEqual("collectWords", driver.Title);
+
+            IWebElement inputElement = driver.FindElement(By.Id("nameTextInput"));
         }
     }
 }
